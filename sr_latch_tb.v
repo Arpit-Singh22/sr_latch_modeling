d9c15024e570c9472eec65dcbd7e337206fe7dc6
Arpit Singh <arpitsingh22@hotmail.com>
2024-05-08 21:49:33 +0530

    SR latch modeling

---
+++ sr_latch_tb.v
@@ -0,0 +1,18 @@
+module sr_latch_tb;
+    reg S,R;    wire Q, Qbar;
+    
+    sr_latch LAT (Q, Qbar, S, R);
+    
+    initial begin
+        $dumpfile("sr_latch_tb.vcd");
+        $dumpvars(0,sr_latch_tb);
+        $monitor($time, "S=%b, R=%b, Q=%b, Qbar=%b",S,R,Q,Qbar);
+
+           S = 1'b0; R = 1'b1;
+        #5 S = 1'b1; R = 1'b1;
+        #5 S = 1'b1; R = 1'b0;
+        #5 S = 1'b1; R = 1'b1;
+        #5 S = 1'b0; R = 1'b0;
+        #5 S = 1'b1; R = 1'b1;
+    end
+endmodule
